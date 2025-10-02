9c3d45ba193b1e8c23b4f915e3be52d0d866bc57
Abul Hassan <imabulhasan99@gmail.com>
2025-10-02 23:26:43 +0600

    Add Vue and Inertia.js dependencies to project

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Welcome to Dual Agent UI</h1>
+    <p>This is a Vue 3 + Inertia.js setup.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome'
+}
+</script>
