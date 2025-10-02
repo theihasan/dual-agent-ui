5cbf4114b534cef4fa11390ebcb267b5faf8de40
Abul Hassan <imabulhasan99@gmail.com>
2025-10-03 01:32:49 +0600

    Add Inertia service provider and testing configuration

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ namespace Ihasan\DualAgentUI\Tests;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Ihasan\DualAgentUI\DualAgentUIServiceProvider;
+use Inertia\ServiceProvider as InertiaServiceProvider;
 
 class TestCase extends Orchestra
 {
@@ -20,6 +21,7 @@ class TestCase extends Orchestra
     protected function getPackageProviders($app)
     {
         return [
+            InertiaServiceProvider::class,
             DualAgentUIServiceProvider::class,
         ];
     }
@@ -27,7 +29,12 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
+        config()->set('inertia.testing.ensure_pages_exist', false);
+        config()->set('inertia.testing.page_paths', []);
+        
+        // Set up view paths for testing
+        $app['view']->addLocation(__DIR__ . '/../resources/views');
+        
         /*
          foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/database/migrations') as $migration) {
             (include $migration->getRealPath())->up();
